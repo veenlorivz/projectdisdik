61967cfbe3313d3f1721ce967244c152309ce932
Herry Prasetyo <herry081288@gmail.com>
2022-06-06 07:43:08 +0700

    devdisdik

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TahunAkademikController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
+Route::prefix('admin')->group(function () {
+    Route::resource('tahun-akademik', TahunAkademikController::class);
+});
