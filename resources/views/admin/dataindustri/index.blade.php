8ec4698cb5fb91bda64cd2be36f94dc8a877a91e
Herry Prasetyo <herry081288@gmail.com>
2022-06-09 06:30:15 +0700

    tambahanmeneh

--- resources/views/admin/dataindustri/index.blade.php
+++ resources/views/admin/dataindustri/index.blade.php
@@ -8,10 +8,29 @@
     <section class="section">
         <div class="section-header">
             <h1>Data Industri</h1>
-
         </div>
         <div class="section-body">
             <h2 class="section-title">Data Industri</h2>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-md">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nama Industri</th>
+                                            <th>Alamat</th>
+                                            <th>No Telp</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
