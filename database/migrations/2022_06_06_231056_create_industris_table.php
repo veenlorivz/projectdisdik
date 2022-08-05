52212c1477b986d056ed52062f11cdf5024e7d1b
veenlorivz <veenlorivz194@gmail.com>
2022-08-05 10:29:09 +0700

    Make schema for industri and jurusan

--- database/migrations/2022_06_06_231056_create_industris_table.php
+++ database/migrations/2022_06_06_231056_create_industris_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('industris', function (Blueprint $table) {
             $table->id();
+            $table->string("nama");
+            $table->string("alamat");
+            $table->string("no_telp");  
             $table->timestamps();
         });
     }
