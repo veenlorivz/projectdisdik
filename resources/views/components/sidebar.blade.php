21514b21c09300e5c0ba1855c3ec11bc90f75ef3
Herry Prasetyo <herry081288@gmail.com>
2022-08-05 09:40:41 +0700

    cobacoba

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,17 +7,14 @@
     </div>
     <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
-        <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-            </ul>
+        <li class="nav-item ">
+            <a href="#" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
+
         </li>
-        <li class="menu-header">Starter</li>
+        <li class="menu-header">Data Master</li>
         <li class="nav-item dropdown">
             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
-                <span>Layout</span></a>
+                <span>Data Master</span></a>
             <ul class="dropdown-menu">
                 <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
                 <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a>
