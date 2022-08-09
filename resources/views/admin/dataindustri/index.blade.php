30d4385b2dd1d8fc023aec78edb763c5b6793d48
veenlorivz <veenlorivz194@gmail.com>
2022-08-09 08:03:44 +0700

    Make data industri page

--- resources/views/admin/dataindustri/index.blade.php
+++ resources/views/admin/dataindustri/index.blade.php
@@ -10,24 +10,26 @@
             <h1>Data Industri</h1>
         </div>
         <div class="section-body">
-            <h2 class="section-title">Data Industri</h2>
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-md">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Nama Industri</th>
-                                            <th>Alamat</th>
-                                            <th>No Telp</th>
-                                        </tr>
-                                    </thead>
-                                </table>
-                            </div>
-                        </div>
+        <div class="card">
+                <div class="card-header">
+                    <h4>Data Industri</h4>
+                </div>
+                <div class="card-body">
+                    <a href="{{ route('sekolah.create') }}" class="btn btn-primary mb-3">Tambah Data Industri</a>
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nama Sekolah</th>
+                                    <th>Alamat</th>
+                                    <th>No Telepon</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
