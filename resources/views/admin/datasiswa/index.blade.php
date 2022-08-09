710eaea5bde04df78fb0e94b34a3b82d30931074
Herry Prasetyo <herry081288@gmail.com>
2022-08-09 09:47:42 +0700

    indexsiswa

--- resources/views/admin/datasiswa/index.blade.php
+++ resources/views/admin/datasiswa/index.blade.php
@@ -15,7 +15,22 @@
                     <h4>Data Siswa</h4>
                 </div>
                 <div class="card-body">
-
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>NISN</th>
+                                    <th>Nama Siswa</th>
+                                    <th>Alamat</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Nama Sekolah</th>
+                                    <th>Nama Jurusan</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
