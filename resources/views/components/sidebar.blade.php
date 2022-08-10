f5f33da0842398d7ae44e5e85831b0760242a28e
Farizirfani <farizirfani29@gmail.com>
2022-08-10 11:13:46 +0700

    Create dashboard admin

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,7 +9,7 @@
     <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
         <li class="nav-item ">
-            <a href="#" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <a href="{{ route('dashboard.index') }}" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
 
         </li>
         <li class="menu-header">Data Master</li>
